9a6b706bc5f7a948c92d00a97c18c675e4e85736
Anthony Thompson <34thompsonas@cua.edu>
2019-06-23 12:54:44 -0400

    First Commit

--- 1_flikr_api_dloader.py
+++ 1_flikr_api_dloader.py
@@ -3,37 +3,37 @@ import flickr_api as Flickr
 import requests
 import json
 
-#https://www.flickr.com/services/api/explore/flickr.photos.search# or use methods
+#https://www.flickr.com/services/api/explore/flickr.photos.search# to get url, or use methods
+
 api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 secret = 'xxxxxxxxxxxxxxxx'
 
 Flickr.set_keys(api_key,secret)
 
 url = ''
+
 response = requests.get(url)
-print(type(response))
-print(response)
-lprint(type(response.json()))#1
-
 data = response.json()
-
 photos = data['photos']['photo']
 
-print('id\t\t\t\t','owner\t\t','title')
-lprint(len(photos))#2
+print(len(photos))
+print(json.dumps(photos[0], indent=4, sort_keys=True))
 
-lprint(json.dumps(photos[0], indent=4, sort_keys=True))
+
 photo=photos[0]
 pic_id = photo['id']
 pic = Flickr.Photo(id=pic_id)
 pSizes = pic.getSizes()
-lprint(json.dumps(pSizes, indent=4, sort_keys=True))
-#lprint(pSizes['Site MP4']['url'])
+print('\n')
+print(json.dumps(pSizes, indent=4, sort_keys=True))
+#print(pSizes['Site MP4']['url'])
+
+print('\t\tid\t\t\t\t','owner\t\t\t','title')
 count=0
 for photo in photos:
     ext = str(count)
     count+=1
-    print(count,photo['id'],photo['owner'],photo['title'])
+    print(str(count)+':  ',photo['id'],photo['owner'],photo['title'])
     pic_id = photo['id']
     pic = Flickr.Photo(id=pic_id)
     pSizes = pic.getSizes()
