9ca979f04720a873bca2d6ac82ad014d0271052f
Anthony Thompson <34thompsonas@cua.edu>
2019-06-23 12:12:17 -0400

    First Commit

---
+++ 1_flikr_api_dloader.py
@@ -0,0 +1,62 @@
+from Space.space import *   #My own little utility package
+import flickr_api as Flickr
+import requests
+import json
+
+#https://www.flickr.com/services/api/explore/flickr.photos.search# or use methods
+api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+secret = 'xxxxxxxxxxxxxxxx'
+
+Flickr.set_keys(api_key,secret)
+
+
+
+
+
+url = ''
+
+response = requests.get(url)
+print(type(response))
+print(response)
+lprint(type(response.json()))#1
+
+data = response.json()
+
+photos = data['photos']['photo']
+
+print('id\t\t\t\t','owner\t\t','title')
+lprint(len(photos))#2
+
+lprint(json.dumps(photos[0], indent=4, sort_keys=True))
+photo=photos[0]
+pic_id = photo['id']
+pic = Flickr.Photo(id=pic_id)
+pSizes = pic.getSizes()
+lprint(json.dumps(pSizes, indent=4, sort_keys=True))
+lprint(pSizes['Site MP4']['url'])
+count=0
+for photo in photos:
+    ext = str(count)
+    count+=1
+    print(count,photo['id'],photo['owner'],photo['title'])
+    pic_id = photo['id']
+    pic = Flickr.Photo(id=pic_id)
+    pSizes = pic.getSizes()
+    print(pSizes['Site MP4']['url'])#####################
+    if "Large" in pSizes:
+        try:
+          pic.save(data_path('chihuahuas\\chihuahua{}'.format(ext)), size_label="Large")  #
+          print('Large jpg Sucess!!!!!')
+        except:
+
+            pass
+    elif "Medium" in pSizes:
+        try:
+          pic.save(data_path('chihuahuas\\chihuahua{}'.format(ext)), size_label="Medium")  # 'HD MP4'
+          print('Medium jpg Sucess!!!!!')
+        except:
+
+            pass
+    else:
+        print('FAIL!!!!!')
+        continue
